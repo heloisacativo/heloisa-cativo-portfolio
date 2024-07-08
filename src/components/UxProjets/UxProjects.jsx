6bc9e96ca0bbec7e24af566f1691958b47a024c3
heloisacativo <heloisacativo@gmail.com>
2024-07-08 15:29:20 -0300

    I Adjusted the project name

--- src/components/UxProjets/UxProjects.jsx
+++ src/components/UxProjets/UxProjects.jsx
@@ -5,14 +5,20 @@ import ImgProjeto3 from "../../assets/imgs/project-3.jpg";
 import "../../components/Main/MediaQueries.css";
 import Footer from "../Footer/Footer";
 import NavigationBar from "../NavigationBar/NavigationBar";
+import { useNavigate } from "react-router-dom";
 
 const UxProjects = () =>{
-    
+
+    const navigate = useNavigate();
+
+    const goToProject = (path) => {
+        navigate(path);
+    }
     return (
         <>
         <NavigationBar/>
         <div className="projectsList">
-                <div className="cards">
+                <div onClick={() => goToProject('/projects/vetWise')} className="cards">
                     <div className="cards card1">
                         <img src={ImgProjeto1} alt="Projeto 1"/>
                         <div className="overlayCard">
@@ -25,7 +31,7 @@ const UxProjects = () =>{
                 </div>
             
                 <div>                
-                    <div className="cards card2">
+                    <div onClick={() => goToProject('/projects/lineNy')} className="cards card2">
                         <img src={ImgProjeto2} alt="Projeto 2"/>
                         <div className="overlayCard">
                         <div className="descriptionCard">
@@ -36,7 +42,7 @@ const UxProjects = () =>{
                     </div>
                 </div>
 
-                <div className="cards">
+                <div onClick={() => goToProject('/projects/kiddo')} className="cards">
                     <div className="cards card3">
                         <img src={ImgProjeto3} alt="Projeto 3"/>
                         <div className="overlayCard">
